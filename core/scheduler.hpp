0b4192a7b3a52b06a625ecf2e553aa61b2e87992
gcrespo1705 <gcrespo1705@gmail.com>
2025-07-05 19:39:02 -0400

    Revert "Implementacion de round robin en el planificador"
    This reverts commit 33ad97fc82856f417259603a609fb2120381b8df.

--- core/scheduler.hpp
+++ core/scheduler.hpp
@@ -4,7 +4,6 @@
 #include "Process.hpp"
 #include "Fcfs.hpp"
 #include "sjf.hpp"
-#include "round_robin.hpp"
 #include <string>
 
 
@@ -36,9 +35,6 @@ void schedule(std::vector<Process>& processes, std::string algorithm, int quantu
             break;
         case SJF:
             runSJF(processes, onStateChange);
-            break;
-        case RR:
-            runRR(processes, quantum, onStateChange);
         default:
             std::cerr<< "Tipo de algoritmo no soportado"<<std::endl;
             break;
